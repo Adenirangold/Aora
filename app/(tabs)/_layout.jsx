00417c5b4f290b31227b752ae382d7a9b7987e0e
MacBookPro <macbookpro@Gold-MacBookPros-Pro.local>
2024-09-17 15:15:27 +0100

    Created onboarding screen

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,111 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+import icons from "../../constants/icons";
+
+const TabIcon = ({ color, icon, name, focused }) => {
+  return (
+    <View className="flex gap-2 items-center justify-center">
+      <Image
+        tintColor={color}
+        resizeMode="contain"
+        source={icon}
+        className="w-6 h-6"
+      ></Image>
+      <Text
+        style={{ color: color }}
+        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs `}
+      >
+        {name}
+      </Text>
+    </View>
+  );
+};
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarActiveTintColor: "#FFA001",
+        tabBarInactiveTintColor: "#CDCDE0",
+        tabBarStyle: {
+          backgroundColor: "#161622",
+          borderTopWidth: 1,
+          borderTopColor: "#232533",
+          height: 90,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="home"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                name="Home"
+                color={color}
+                icon={icons.home}
+              ></TabIcon>
+            );
+          },
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="bookmark"
+        options={{
+          title: "Bookmark",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                name="Bookmark"
+                color={color}
+                icon={icons.bookmark}
+              ></TabIcon>
+            );
+          },
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="create"
+        options={{
+          title: "Create",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                name="Create"
+                color={color}
+                icon={icons.plus}
+              ></TabIcon>
+            );
+          },
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => {
+            return (
+              <TabIcon
+                focused={focused}
+                name="Profile"
+                color={color}
+                icon={icons.profile}
+              ></TabIcon>
+            );
+          },
+        }}
+      ></Tabs.Screen>
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
---
+++ app/index.jsx
@@ -0,0 +1,51 @@
+import { Image, ScrollView, Text, View } from "react-native";
+import React from "react";
+import { Link, router } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "../constants/images";
+import CustomButton from "../components/CustomButton";
+import { StatusBar } from "expo-status-bar";
+
+export default function index() {
+  return (
+    <SafeAreaView className="h-full bg-primary">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full items-center justify-center min-h-[85vh] px-4">
+          <Image
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+            source={images.logo}
+          ></Image>
+          <Image
+            className="max-w-[380px] w-full h-[300px]"
+            source={images.cards}
+            resizeMode="contain"
+          ></Image>
+          <View className="relative mt-8">
+            <Text className="text-3xl text-white font-bold text-center">
+              Discover Endless Possibilities With{"  "}
+              <Text className="text-secondary-200">Aora</Text>
+            </Text>
+            <Image
+              className="absolute w-[136px] h-[15px] -bottom-2 -right-8"
+              resizeMode="contain"
+              source={images.path}
+            ></Image>
+          </View>
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center">
+            Where creativity meets innovation embark on a journey of limitless
+            exploration with Aora
+          </Text>
+          <CustomButton
+            handlePress={() => {
+              router.push("/signIn");
+            }}
+            title="Continue With Email"
+            containerStyles="w-full mt-7"
+          ></CustomButton>
+        </View>
+      </ScrollView>
+      <StatusBar backgroundColor="#161622" style="light"></StatusBar>
+    </SafeAreaView>
+  );
+}
